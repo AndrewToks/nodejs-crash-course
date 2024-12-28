50dafadc2b1e9620b721ed91dc086e34a9493aa0
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2024-12-28 23:09:42 +0100

    directories

--- nodejs-basics/file-system/files.js
+++ nodejs-basics/file-system/files.js
@@ -26,12 +26,23 @@ const fs = require('fs');
 // })
 
 //directories
-fs.mkdir('./assets',(err)=>{
-    if(err){
-        console.log(err);
-    }
-    console.log('folder created.');
-    
-})
+if(!fs.existsSync('./assets')){
+    fs.mkdir('./assets',(err)=>{
+        if(err){
+            console.log(err);
+        }
+        console.log('folder created.');
+    })
+} else{
+    fs.rmdir('./assets',(err)=>{
+        if (err) {
+            console.log(err); 
+        }
+        else{
+            console.log('folder deleted');
+            
+        }
+    })
+}
 
 //deleting files
