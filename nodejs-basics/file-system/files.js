b107e814665df2a7f7da7a1b120923d03b070ff7
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2024-12-28 23:24:53 +0100

    deleting files

--- nodejs-basics/file-system/files.js
+++ nodejs-basics/file-system/files.js
@@ -26,23 +26,35 @@ const fs = require('fs');
 // })
 
 //directories
-if(!fs.existsSync('./assets')){
-    fs.mkdir('./assets',(err)=>{
-        if(err){
-            console.log(err);
-        }
-        console.log('folder created.');
-    })
-} else{
-    fs.rmdir('./assets',(err)=>{
-        if (err) {
-            console.log(err); 
-        }
-        else{
-            console.log('folder deleted');
+// if(!fs.existsSync('./assets')){
+//     fs.mkdir('./assets',(err)=>{
+//         if(err){
+//             console.log(err);
+//         }
+//         console.log('folder created.');
+//     })
+// } else{
+//     fs.rmdir('./assets',(err)=>{
+//         if (err) {
+//             console.log(err); 
+//         }
+//         else{
+//             console.log('folder deleted');
             
-        }
-    })
-}
+//         }
+//     })
+// }
+
+
 
 //deleting files
+
+if(fs.existsSync('./docs/deleteme.txt')){
+    fs.unlink('./docs/deleteme.txt',(err)=>{
+        if(err){
+            console.log(err);
+        }   
+        console.log('File deleted');
+        
+    })
+}
