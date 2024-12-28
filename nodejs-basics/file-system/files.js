a6d7b0d824c84a652d4aae6ea5461a99e8159a70
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2024-12-28 22:37:17 +0100

    file-system

--- nodejs-basics/file-system/files.js
+++ nodejs-basics/file-system/files.js
@@ -2,7 +2,12 @@ const fs = require('fs');
 
 //reading files
 //This takes two arguments
-fs.readFile('./docs/blog1.txt',()=>{
+fs.readFile('./docs/blog1.txt',(err,data)=>{
+    if(err){
+        console.log(err);
+    }
+    console.log(data.toString());//it prints buffer to the 
+    // console but you use toString() method to convert to string
     
 })
 
