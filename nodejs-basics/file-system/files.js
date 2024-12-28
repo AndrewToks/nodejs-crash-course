e9c6912c02a010cafa6c66903e7012439da89094
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2024-12-28 22:58:46 +0100

    directories

--- nodejs-basics/file-system/files.js
+++ nodejs-basics/file-system/files.js
@@ -1,20 +1,37 @@
 const fs = require('fs');
 
+//asynchronous -> takes time to run
+
 //reading files
 //This takes two arguments
-fs.readFile('./docs/blog1.txt',(err,data)=>{
+// fs.readFile('./docs/blog1.txt',(err,data)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(data.toString());//it prints buffer to the 
+//     // console but you use toString() method to convert to string
+// })
+
+// console.log('last line');
+
+
+//writing files
+//three arguments
+// fs.writeFile('./docs/blog1.txt','Hello World',()=>{
+//     console.log('file was written');
+// })
+
+// fs.writeFile('./docs/blog2.txt','Hello World',()=>{
+//     console.log('file was written');
+// })
+
+//directories
+fs.mkdir('./assets',(err)=>{
     if(err){
         console.log(err);
     }
-    console.log(data.toString());//it prints buffer to the 
-    // console but you use toString() method to convert to string
+    console.log('folder created.');
     
 })
-
-//writing files
-
-
-//directories
-
 
 //deleting files
